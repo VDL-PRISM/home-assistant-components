906b18172f4b9d8490c86f8f776b9397533a1632
Philip Lundrigan <philiplundrigan@gmail.com>
2017-06-14 12:50:09 -0600

    Add sleep time back

--- sensor/prisms_wifi_sensor.py
+++ sensor/prisms_wifi_sensor.py
@@ -279,6 +279,7 @@ def get_data(device, batch_size, max_data_transferred):
                 _LOGGER.info(
                     "%s - %s: Stopping because acks (%s) != size (%s)",
                     device.name, device.address, device.ack, batch_size)
+                time.sleep(5)
                 break
 
             # Let's give the system some time to catch up
@@ -290,6 +291,7 @@ def get_data(device, batch_size, max_data_transferred):
                 _LOGGER.info(
                     "%s - %s: Stopping because total_packets (%s) > %s",
                     device.name, device.address, total_packets, max_data_transferred)
+                time.sleep(5)
                 break
 
     except Exception:
